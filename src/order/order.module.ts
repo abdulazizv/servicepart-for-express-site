d67431bb599c028431e02359e98d836f8b33de59
abdulazizv <abdulazizheir@gmail.com>
2023-03-20 16:05:27 +0500

    changes

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Order } from './entities/order.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Order])],
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService],
 })
 export class OrderModule {}
